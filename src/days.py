7893f6aa2c27b65b878186e2061b4ad8cb095a3a
Nono Saha Cyrille Merleau <lemerleau@Nonos-Laptop.local>
2023-04-13 15:51:20 +0200

    Commit on the src code day 2

--- src/days.py
+++ src/days.py
@@ -1,10 +1,19 @@
-import numpy as np
+import sys
 
 
+def hello(name) :
+    print ("Hello {} and welcome to the Int. Comp. for DS".format(name))
 
 
 def main():
-    print("Hello world")
+    if len(sys.argv) > 1 :
+        name = str(sys.argv[1])
+    else :
+        name = "<NO NAME>"
+        print("Program called without arguments. Please, specify your name as the first argument.")
+
+    # Call my first function here
+    hello(name)
 
 
 
